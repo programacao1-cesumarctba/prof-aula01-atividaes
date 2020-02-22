8c119ad523bbfc61fb19d99a86d17ca8deb47f75
Maycon de Moraes <mayconmoraes.dm@gmail.com>
2020-02-21 21:53:45 -0300

    Initial commit
    Commit contendo a base do projeto e o arquivo atividades.md com os exercícios a serem desenvolvidos.

---
+++ src/com/mayconmoraesdm/atividades/Desafio1.java
@@ -0,0 +1,8 @@
+package com.mayconmoraesdm.atividades;
+
+public class Desafio1 {
+	public void teste() {
+		System.out.print("Teste DESAFIO 1");
+	}
+
+}
---
+++ src/com/mayconmoraesdm/main/Programa.java
@@ -0,0 +1,11 @@
+package com.mayconmoraesdm.main;
+
+import com.mayconmoraesdm.atividades.Desafio1;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		new Desafio1().teste();
+	}
+
+}
